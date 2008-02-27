2f1c8134fbc5a1ab11471c96fb4e657804f2da04
Gerard Lledo <gerard.lledo@gmail.com>
2008-02-27 22:33:28 +0200

    Changing to 8-space tabs (more kernel, diff and cat like...)

--- mcoil.c
+++ mcoil.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-#define MAX_LEN_PATH 	256
+#define MAX_LEN_PATH 		256
 #define INC_AREA 		(++area_count)
 #define DEC_AREA		(--area_count)
 #define PREV_AREA 		(area_count-1)
 #define NEXT_AREA		(area_count+1)
-#define AREA_BLOCKED 	(-1)
+#define AREA_BLOCKED 		(-1)
 #define AREA_CLEAR 		0
 
-#define AREA_NOW(t)				(t.field[t.x][t.y])
-#define AREA_RIGHT(t)			(t.field[t.x+1][t.y])
-#define AREA_DOWN(t)			(t.field[t.x][t.y+1])
-#define AREA_LEFT(t)			(t.field[t.x-1][t.y])
-#define AREA_UP(t)				(t.field[t.x][t.y-1])
+#define AREA_NOW(t)		(t.field[t.x][t.y])
+#define AREA_RIGHT(t)		(t.field[t.x+1][t.y])
+#define AREA_DOWN(t)		(t.field[t.x][t.y+1])
+#define AREA_LEFT(t)		(t.field[t.x-1][t.y])
+#define AREA_UP(t)		(t.field[t.x][t.y-1])
 
 #define CHECK_BOUNDS_RIGHT(t) 	(t.x+1 >= t.size_x)
 #define CHECK_BOUNDS_DOWN(t) 	(t.y+1 >= t.size_y)
 #define CHECK_BOUNDS_LEFT(t) 	(t.x-1 >= t.size_x)
-#define CHECK_BOUNDS_UP(t)	 	(t.y-1 >= t.size_y)
+#define CHECK_BOUNDS_UP(t)	(t.y-1 >= t.size_y)
 
-#define MOVE_RIGHT(t)			(t.x++)
-#define MOVE_DOWN(t)			(t.y++)
-#define MOVE_LEFT(t)			(t.x--)
-#define MOVE_UP(t)				(t.y--)
+#define MOVE_RIGHT(t)		(t.x++)
+#define MOVE_DOWN(t)		(t.y++)
+#define MOVE_LEFT(t)		(t.x--)
+#define MOVE_UP(t)		(t.y--)
 
 enum moves {
 	PLAY_LEFT = 1,
